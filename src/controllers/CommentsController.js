1f08f1d083f93840ad681e63fdaf045ce760e9ba
Jake Overall <joverall22@gmail.com>
2025-02-24 14:33:21 -0700

    basic setup

---
+++ src/controllers/CommentsController.js
@@ -0,0 +1,45 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { valuesService } from '../services/ValuesService.js'
+import BaseController from '../utils/BaseController.js'
+
+const comments = {
+  'videoId1': []
+}
+
+
+export class CommentsController extends BaseController {
+  constructor() {
+    super('api/comments')
+    this.router
+      .get('/:videoId', this.getCommentsByVideoId)
+      // NOTE: Beyond this point all routes require Authorization Bearer token
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createComment.bind(this))
+  }
+
+  async getCommentsByVideoId(request, response, next) {
+    try {
+      const videoId = request.params.videoId
+      response.send(comments[videoId] || [])
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createComment(request, response, next) {
+    try {
+      const comment = request.body
+      // NOTE NEVER TRUST THE CLIENT TO ADD THE CREATOR ID
+      comment.creatorId = request.userInfo.id
+
+      comments[comment.videoId] = comments[comment.videoId] || []
+      comments[comment.videoId].push(comment)
+
+      response.send(comment)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+
+}
